e01fbd93aaa22529be433ae72ce9d113be98633f
archidowna16 <carlosarchidona16@gmail.com>
2025-07-27 20:58:06 +0200

    Método para añadir un menú principal con seleccón de juegos

--- src/Gestor.java
+++ src/Gestor.java
@@ -1,4 +1,24 @@
 import java.util.*;
 public class Gestor {
 
+    Scanner sc = new Scanner(System.in);
+
+    public void menu(){
+        int opcion = 0;
+
+        do{
+            System.out.println(" -- SimpleLauncherGame --");
+            System.out.println("Elige un juego: ");
+            System.out.println("1. Adivina el número");
+            System.out.println("0. Salir del programa");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion){
+                case 0 -> System.err.println("Saliendo del programa...");
+                case 1 -> System.out.println();
+                default -> System.out.println("Introduce una opción válida");
+            }
+        } while (opcion != 0);
+    }
 }
