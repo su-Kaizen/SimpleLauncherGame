096ea4398ad179275c0e85bf7a553d4f53b2f5bc
archidowna16 <carlosarchidona16@gmail.com>
2025-07-27 21:32:08 +0200

    Añadido método adivinar() e integrado en el case 1 del menú

--- src/Gestor.java
+++ src/Gestor.java
@@ -16,9 +16,52 @@ public class Gestor {
 
             switch (opcion){
                 case 0 -> System.err.println("Saliendo del programa...");
-                case 1 -> System.out.println();
+                case 1 -> adivinar();
                 default -> System.out.println("Introduce una opción válida");
             }
         } while (opcion != 0);
+    }
+
+    public void adivinar(){
+        int intentosJugador;
+        int intentosRestantes = 5;
+        boolean resultado = false;
+
+        System.out.println("**  ADIVINA EL NÚMERO  **");
+
+        System.out.println("Introduce el valor mínimo del rango: ");
+        int min = sc.nextInt();
+
+        System.out.println("Introduce el valor máximo del rango: ");
+        int max = sc.nextInt();
+
+        while (max <= min){
+            System.out.println("El valor máximo debe de ser mayor que el mínimo. Inténtalo de nuevo");
+            System.out.println("Introduce el valor máximo del rango: ");
+            max = sc.nextInt();
+        }
+
+        int numeroSecreto = (int) (Math.random() * (max - min +1)) + min;
+
+        System.out.println("!Intenta adivinar el número entre " + min + " y " + max + "!");
+        System.out.println("Tienes 5 intentos");
+
+        while (intentosRestantes > 0 && resultado == false){
+            System.out.print("Intento (" + (6 - intentosRestantes) + "/5): ");
+            intentosJugador = sc.nextInt();
+            intentosRestantes--;
+            if (intentosJugador < numeroSecreto){
+                System.out.println("El número es más alto");
+            } else if (intentosJugador > numeroSecreto) {
+                System.out.println("El número es más bajo");
+            } else{
+                System.out.println("¡Correcto! Has adivinado el número");
+                resultado = true;
+            }
+        }
+
+        if (resultado == false){
+            System.out.println("Te has quedado sin intentos. El número secreto era el: "+ numeroSecreto);
+        }
     }
 }
