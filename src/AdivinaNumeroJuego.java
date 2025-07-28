95577909bc85e1be1fa2a1c19bf10c975b77eed0
su-kaizen <badboynodo313@gmail.com>
2025-07-28 15:25:03 +0300

    A partir de ahora cada juego irá en su propia clase con sus métodos estáticos

---
+++ src/AdivinaNumeroJuego.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class AdivinaNumeroJuego {
+    public static Scanner sc = new Scanner(System.in);
+    public static void adivinar(){
+        int intentosJugador;
+        int intentosRestantes = 5;
+        boolean resultado = false;
+
+        System.out.println("**  ADIVINA EL NÚMERO  **");
+
+        System.out.println("Introduce el valor mínimo del rango: ");
+        int min = sc.nextInt();
+
+        System.out.println("Introduce el valor máximo del rango: ");
+        int max = sc.nextInt();
+
+        while (max <= min){
+            System.out.println("El valor máximo debe de ser mayor que el mínimo. Inténtalo de nuevo");
+            System.out.println("Introduce el valor máximo del rango: ");
+            max = sc.nextInt();
+        }
+
+        int numeroSecreto = (int) (Math.random() * (max - min +1)) + min;
+
+        System.out.println("!Intenta adivinar el número entre " + min + " y " + max + "!");
+        System.out.println("Tienes 5 intentos");
+
+        while (intentosRestantes > 0 && resultado == false){
+            System.out.print("Intento (" + (6 - intentosRestantes) + "/5): ");
+            intentosJugador = sc.nextInt();
+            intentosRestantes--;
+            if (intentosJugador < numeroSecreto){
+                System.out.println("El número es más alto");
+            } else if (intentosJugador > numeroSecreto) {
+                System.out.println("El número es más bajo");
+            } else{
+                System.out.println("¡Correcto! Has adivinado el número");
+                resultado = true;
+            }
+        }
+
+        if (resultado == false){
+            System.out.println("Te has quedado sin intentos. El número secreto era el: "+ numeroSecreto);
+        }
+    }
+}
