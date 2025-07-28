8f499b3427e3832989a80e9306fdf4f3b46beb99
su-kaizen <badboynodo313@gmail.com>
2025-07-28 19:39:00 +0300

    Se ha añadido la logica de que el usuario ponga una ficha en el tablero

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 public class TicTacToe {
     private String[][] tablero;
+    private int[] c;
     public static final String BLUE = "\033[0;34m";
     public static final String RED = "\033[0;31m";
     public static final String END = "\033[0m";
 
     public TicTacToe(){
         tablero = new String[][]{{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+        c = new int[2];
     }
 
     public static void startGame(){
@@ -21,38 +23,68 @@ public class TicTacToe {
         String choice = Gestor.stringInput();
 
         if(choice.equalsIgnoreCase("s")){
+            /*
+            * -1 = empate
+            * 0 = en juego
+            * 1 = jugador gana
+            * 2 = bot gana
+            * */
+            int winner = 0;
+
             do{
-                System.out.println("Elige posición: ");
-                posicion = Gestor.intInput();
+                // juega el usuario
+                do{
+                    System.out.println("Elige posición: ");
+                    posicion = Gestor.intInput();
 
-                valido = t.comprobarPosicion(posicion);
+                    valido = t.comprobarPosicion(posicion);
 
-                System.out.print(!valido ? "\n Porfavor introduzca una coordenada valida y vacia..." : "");
+                    if(valido){
+                        t.ponerFicha("X");
+                    }
+                    else{
+                        System.out.print("\n Porfavor introduzca una coordenada valida y vacia...");
+                    }
+                }
+                while(!valido);
+
+                t.pintarTablero();
 
             }
-            while(!valido);
-
+            while(winner == 0);
         }
-
     }
 
 
     public boolean comprobarPosicion(int pos){
         pos = pos-1;
         int x = 0;
+
         // Si la posicion esta fuera de esos rangos directamente es una posicion invalida
         if(pos > 9 || pos < 0){
             return false;
         }
 
         // Sacando la posicion horizontal y vertical
+        sacarPosicion(pos);
+
+        // Si no tiene ni X ni 0 es que es libre, por lo tanto devolverá true
+        return !this.tablero[c[0]][c[1]].equals("X") && !this.tablero[c[0]][c[1]].equals("0");
+    }
+
+    public void sacarPosicion(int pos){
+        int x = 0;
         while(pos-3 >= 0){
             pos = pos - 3;
             x ++;
         }
+        c[0] = x;
+        c[1] = pos;
+    }
 
-        // Si no tiene ni X ni 0 es que es libre, por lo tanto devolverá true
-        return !this.tablero[x][pos].equals("X") && !this.tablero[x][pos].equals("0");
+
+    public void ponerFicha(String ficha){
+        tablero[c[0]][c[1]] = ficha;
     }
 
     public void pintarTablero(){
