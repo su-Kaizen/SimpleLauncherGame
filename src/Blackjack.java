7a1e4be611438609c8c4e9ece0f1238d450e4fc4
su-kaizen <badboynodo313@gmail.com>
2025-07-31 20:53:47 +0300

    el juego ya tiene funcionalidad completa, aunque no está añadido al menu

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -11,47 +11,59 @@ public class Blackjack {
         cartasDealer = new ArrayList<>();
         cartasJugador = new ArrayList<>();
         baraja.remover();
+        puntuacionDealer = 0;
+        puntuacionJugador = 0;
     }
 
     public static void startGame(){
-        /*
-         * -1 = Empate
-         * 0 = En juego
-         * 1 = Gana el jugador
-         * 2 = Gana del dealer
-         * */
-        int status = 0;
+        Gestor.clear();
+        String jugarDenuevo = "n";
         Blackjack b = new Blackjack();
         System.out.println("BLACKJACK\nTrata de llegar a 21 sin pasarte");
         System.out.println("Presiona enter para empezar a jugar.");
         Gestor.stringInput();
         System.out.println();
 
-        b.cartasDealer.add(b.baraja.sacarPrimera());
-
-        b.cartasJugador.add(b.baraja.sacarPrimera());
-        b.cartasJugador.add(b.baraja.sacarPrimera());
-        b.imprimirMesa();
-        do{
-
-            if(b.puntuacionJugador < 21){
-                int choice = b.imprimirOpciones();
-                if(choice == 1){
-                    b.cartasJugador.add(b.baraja.sacarPrimera());
-                }
-                else{
-                    break;
-                }
-            }
-            b.imprimirMesa();
-        }
-        while(b.puntuacionJugador < 21);
-
         do{
             b.cartasDealer.add(b.baraja.sacarPrimera());
+
+            b.cartasJugador.add(b.baraja.sacarPrimera());
+            b.cartasJugador.add(b.baraja.sacarPrimera());
             b.imprimirMesa();
+            do{
+
+                if(b.puntuacionJugador < 21){
+                    int choice = b.imprimirOpciones();
+                    if(choice == 1){
+                        b.cartasJugador.add(b.baraja.sacarPrimera());
+                    }
+                    else{;
+                        break;
+                    }
+                }
+                Gestor.clear();
+                b.imprimirMesa();
+            }
+            while(b.puntuacionJugador < 21);
+
+            Gestor.clear();
+            do{
+                b.cartasDealer.add(b.baraja.sacarPrimera());
+                b.imprimirMesa();
+                Gestor.sleep(2);
+                Gestor.clear();
+            }
+            while (b.puntuacionDealer < 17);
+            b.imprimirMesa();
+            b.comprobarGanador();
+
+            System.out.println("\nJugar de nuevo?[S/n]");
+            jugarDenuevo = Gestor.stringInput();
+            if(jugarDenuevo.equalsIgnoreCase("s")){
+               b = new Blackjack();
+            }
         }
-        while (b.puntuacionDealer < 17);
+        while(jugarDenuevo.equalsIgnoreCase("s"));
     }
 
 
@@ -63,7 +75,8 @@ public class Blackjack {
     }
 
     public void imprimirMesa(){
-        System.out.println();
+
+        System.out.println("+-----------------------------------------------------+");
         System.out.print("DEALER -> ");
         puntuacionDealer = 0;
         for(Carta c: cartasDealer){
@@ -71,7 +84,7 @@ public class Blackjack {
             puntuacionDealer += c.getValor();
         }
         System.out.println("| El dealer tiene "+puntuacionDealer+". "+(puntuacionDealer > 21 ? "Se ha pasado." : ""));
-        System.out.println("+---------------------------------------+");
+
         System.out.print("JUGADOR -> ");
         puntuacionJugador = 0;
         for(Carta c: cartasJugador){
@@ -79,6 +92,21 @@ public class Blackjack {
             puntuacionJugador += c.getValor();
         }
         System.out.println("| Tienes "+puntuacionJugador+". " + (puntuacionJugador > 21 ? "Te has pasado." : ""));
+        System.out.println("+-----------------------------------------------------+");
         System.out.println();
+    }
+
+    public void comprobarGanador(){
+        puntuacionJugador = puntuacionJugador > 21 ? -1 : puntuacionJugador;
+        puntuacionDealer = puntuacionDealer > 21 ? -1 : puntuacionDealer;
+        if(puntuacionJugador > puntuacionDealer){
+            System.out.println("Has ganado!!");
+        }
+        else if(puntuacionJugador < puntuacionDealer){
+            System.err.println("Has perdido");
+        }
+        else{
+            System.out.println("Empate.");
+        }
     }
 }
