d4b5dc041897944b7d2738b13104464bd9c59009
archidowna16 <carlosarchidona16@gmail.com>
2025-08-07 22:50:26 +0200

    En vez inicializar los atributos en el constructor, hacerlo sin constructores

--- src/JuegoDado.java
+++ src/JuegoDado.java
@@ -1,26 +1,20 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class JuegoDado {
-    private static Dado d1;
-    private static Scanner sc;
+    private static Dado d1 = new Dado();
+    private static Scanner sc = new Scanner(System.in);
     private static boolean comprobar = true;
 
-    public JuegoDado(){
-        d1 = new Dado();
-        sc = new Scanner(System.in);
-    }
-
-    public static void juegoDado(){
+    public static void juegoDado() {
         System.out.println(" ** JUEGO DEL DADO **");
 
-        while (comprobar){
-            System.out.println("Presiona ENTER para tirar el dado o escribe 'salir' para terminar: ");
+        while (comprobar) {
+            System.out.print("Presiona ENTER para tirar el dado o escribe 'salir' para terminar: ");
             String entrada = sc.nextLine();
 
-            if (entrada.equalsIgnoreCase("salir")){
+            if (entrada.equalsIgnoreCase("salir")) {
                 System.err.println("Saliendo del juego....");
                 comprobar = false;
-            } else{
+            } else {
                 d1.lanzar();
                 System.out.println("Ha salido el número " + d1.getNumero());
                 d1.imprimir();
